b80db3a9d9ebe0ad816034572550e8cfddd8d0b8
Keisuke-Suzuki229 <154766861+Keisuke-Suzuki229@users.noreply.github.com>
2024-04-04 20:16:40 +1300

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18{
+    public KatoHanako_Chapter18(){
+        super();
+        
+    }
+     public void  setGivenName(){
+        givenName = "花子";   
+        System.out.println("名前は"+ familyName + givenName + "です");
+    }
+    public void eachIntroduce(){
+        System.out.println("趣味は読書です");
+    
+    }
+
+}
+
